e89098e8e6a554517405d1f28f0173b369d08ba9
DixonOrtiz <dixon.ortizc@gmail.com>
2020-01-28 17:33:03 -0300

    Idea inicial del proyecto terminada, abierto a futuros features

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,19 +3,36 @@ import React from 'react';
 class Card extends React.Component {
   constructor(props) {
     super(props);
-    this.cardHeaderValue = 'card-header bg-danger';
+    this.cardValorHeader = 'card-header bg-danger';
+
     this.state = {
       vidas: 0,
     };
   }
 
+  sumar = () => {
+    let vidasMasUno = this.state.vidas + 1;
+    this.setState({
+      vidas: vidasMasUno,
+    });
+  };
+
+  restar = () => {
+    if (this.state.vidas > 0) {
+      let vidasMenosUno = this.state.vidas - 1;
+      this.setState({
+        vidas: vidasMenosUno,
+      });
+    }
+  };
+
   render() {
-    if (this.props.nombre === 'Luigi') this.cardHeaderValue = 'card-header bg-success';
+    if (this.props.nombre === 'Luigi') this.cardValorHeader = 'card-header bg-success';
 
     return (
       <div className="col-md-3">
         <div className="card mt-4">
-          <div className={this.cardHeaderValue}>
+          <div className={this.cardValorHeader}>
             <h3 class="text-white">{this.props.nombre}</h3>
             <span className="badge badge-pill badge-danger ml-2">{}</span>
           </div>
@@ -29,10 +46,10 @@ class Card extends React.Component {
                 </span>
               </li>
               <li className="list-group-item">
-                <button id="sumarVida" class="btn btn-sq-xs btn-success ml-2">
+                <button onClick={this.sumar} class="btn btn-sq-xs btn-success ml-2">
                   +1
                 </button>
-                <button id="restarVida" class="btn btn-sq-xs btn-danger ml-2">
+                <button onClick={this.restar} class="btn btn-sq-xs btn-danger ml-2">
                   -1
                 </button>
               </li>
